2345ecc15efce895b8308a1ff42ea4d50f6385b5
akhilreddyjirra <akhilreddyjirra@users.noreply.github.com>
2018-07-15 17:01:12 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in $(pwd)/*.deb
 do
-    curl -u $admin:$password --upload-file "${i##*/}" http://139.59.74.184/nexus/repository/prox-x/frontend/20180714-09/
+    curl -u $admin:$password --upload-file "${i##*/}" http://139.59.74.184/nexus/repository/prox-x/frontend/$verision/
 
 done
